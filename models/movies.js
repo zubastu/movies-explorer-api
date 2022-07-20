725f4e36fed676303a47e9faf35b4a3bd3dede45
zubastu <79757684+zubastu@users.noreply.github.com>
2022-07-20 14:34:31 +0300

    Добавил модель Фильма, настроил валидироваие url

--- models/movies.js
+++ models/movies.js
@@ -1,6 +1,68 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const movie = new mongoose.Schema({
+const validUrl = (value) => {
+  if (!validator.isURL(value)) {
+    throw new Error('Поле должно содержать корректную ссылку');
+  }
+};
 
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validUrl,
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validUrl,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validUrl,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
 });
+
+module.exports = mongoose.model('movie', movieSchema);
