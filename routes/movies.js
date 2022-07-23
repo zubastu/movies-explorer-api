3a80200605298249d58d3674341da5644285986f
zubastu <79757684+zubastu@users.noreply.github.com>
2022-07-23 11:44:32 +0300

    Delete routes directory

--- routes/movies.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
-const { movieValidation, movieDeleteValidation } = require('../validation/validation');
-
-router.get('/', getMovies);
-router.post('/', movieValidation, createMovie);
-router.delete('/:movieId', movieDeleteValidation, deleteMovie);
-
-module.exports = router;
--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getUser, patchUser } = require('../controllers/users');
-const { patchUserValidation } = require('../validation/validation');
-
-router.get('/me', getUser);
-router.patch('/me', patchUserValidation, patchUser);
-
-module.exports = router;
