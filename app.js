845a5fce6b61b338151d4d4a99879ece98102b43
zubastu <79757684+zubastu@users.noreply.github.com>
2022-07-24 11:11:38 +0300

    добавил режимы работы mongo.connect(dev/production)

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const { errorProcessing } = require('./middlewares/errors');
-const { MONGOOSE_URL, PORT } = require('./utils/constants');
+const {
+  MONGOOSE_URL,
+  PORT,
+  MONGO_DEV,
+  NODE_ENV,
+} = require('./utils/constants');
+
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 const limiter = require('./middlewares/limiter');
@@ -14,7 +19,7 @@ const routes = require('./routes/index');
 
 const app = express();
 
-mongoose.connect(MONGOOSE_URL, { useNewUrlParser: true, family: 4 });
+mongoose.connect(NODE_ENV === 'production' ? MONGOOSE_URL : MONGO_DEV, { useNewUrlParser: true, family: 4 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
