d53cabb22d3139e21ef95cc4a2ef64f3cfabd806
zubastu <79757684+zubastu@users.noreply.github.com>
2022-07-24 15:24:21 +0300

    Исправил Joi password

--- validation/validation.js
+++ validation/validation.js
@@ -11,6 +11,7 @@ module.exports.registrationValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     name: Joi.string().required().min(2).max(30),
+    password: Joi.string().required(),
   }),
 });
 
