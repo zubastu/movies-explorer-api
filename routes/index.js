9d2962385bf63220f63d3392aa1be222fb55b891
zubastu <79757684+zubastu@users.noreply.github.com>
2022-07-24 15:30:43 +0300

    Исправил обработку неизвестных URL

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { register, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const { loginValidation, registrationValidation } = require('../validation/validation');
-const { NOT_FOUND } = require('../middlewares/errors');
+const NotFound = require('../errors/NotFound');
 
 router.post('/signup', registrationValidation, register);
 router.post('/signin', loginValidation, login);
@@ -10,6 +10,6 @@ router.use(auth);
 router.use('/users', require('./users'));
 router.use('/movies', require('./movies'));
 
-router.use((req, res) => res.status(NOT_FOUND).send({ message: 'Не найдено' }));
+router.use('*', (req, res, next) => next(new NotFound('Страница не найдена')));
 
 module.exports = router;
