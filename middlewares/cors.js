f940bb8a9c299294d42840d5ac534e532a144fc0
zubastu <79757684+zubastu@users.noreply.github.com>
2022-08-16 21:29:54 +0300

    вернул защиту корс

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,11 +1,9 @@
-/*
  const allowedCors = [
   'https://movies.explorer.zubastu.nomoredomains.sbs/',
   'http://movies.explorer.zubastu.nomoredomains.sbs/',
   'http://localhost:3000',
   'https://localhost:3000',
 ];
-*/
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
@@ -16,9 +14,9 @@ module.exports = (req, res, next) => {
 
   res.header('Access-Control-Allow-Credentials', true);
 
-/*   if (allowedCors.includes(origin)) { */
-    res.header('Access-Control-Allow-Origin', '*');
-/*   } */
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
